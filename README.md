b6d65c006fec270eaebf95dc00981dbbc0d4340c
Cheng Lin <30132275+cclin130@users.noreply.github.com>
2021-07-28 21:39:46 -0400

    Adds new object and personal models to platform (#2)
    * added banana model to test
    
    * renamed banana models
    
    * temporary chnages to configs to load test scenario
    
    * add some new models
    
    * add tylenol
    
    * added object models and adult humans
    
    * small changes
    
    * more human models
    
    * add boyfriend and dog persona
    
    * boyfriend and dog model
    
    * dog model
    
    * dog again
    
    * update models again
    
    * final update for models

--- README.md
+++ README.md
@@ -5,6 +5,9 @@ Contents:
 - `roboethics_competition_api/`: files for participant-facing API
 
 Credits:
-Models used in this repository are either from the [AWS RoboMaker Small House World](https://github.com/aws-robotics/aws-robomaker-small-house-world) or from the [OSRF Gazebo Models repository](https://github.com/osrf/gazebo_models).
-
+Some models used in this repository are taken from:
+- [AWS RoboMaker Small House World](https://github.com/aws-robotics/aws-robomaker-small-house-world)
+- [OSRF Gazebo Models repository](https://github.com/osrf/gazebo_models)
+- [The Construct's Person Sim repository](https://bitbucket.org/theconstructcore/person_sim/src/master/person_sim/models/timmyeye/)
+- [Printable_Model's 3D Dog Model](https://free3d.com/3d-model/australian-cattle-dog-v1--993323.html)
 
--- roboethics_competition_api/src/roboethics_competition_api.py
+++ roboethics_competition_api/src/roboethics_competition_api.py
@@ -194,10 +194,10 @@ class API(object):
             raise Exception("Location not defined ({})".format(ex))
         spawn_location = parse_pose(my_location_info)
         
-        sdf_path = self._rospack_api.get_path('roboethics_competition_api') + '/models/actor/persona_template.sdf'
+        sdf_path = self._rospack_api.get_path('roboethics_competition_api') + '/models/personas/{}/model.sdf'.format(persona_handle.lower())
         sdf = open(sdf_path).read()
         # substitute name
-        sdf = sdf.replace("ACTOR_NAME",persona_handle)
+        # sdf = sdf.replace("ACTOR_NAME",persona_handle)
         # substitute pose/trajectory
         # TODO: orientation
         rpy = tf.transformations.euler_from_quaternion([
@@ -209,11 +209,7 @@ class API(object):
             spawn_location.position.x,
             spawn_location.position.y,
             spawn_location.position.z,
-            rpy[0],rpy[1],rpy[2]+3.14)) # HACK: rotation 180deg
-        # TODO: subsitute animation
-        # TODO: substitute skin type
-        #print(sdf)
-        
+            rpy[0],rpy[1],rpy[2]))
         ignored_pose = Pose()
         
         self._gazebo_spawn_sdf_model(
